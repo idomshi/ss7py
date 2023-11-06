7806190bdc41c41e7db93d3c82b1991a428dee02
idomshi <56523771+idomshi@users.noreply.github.com>
2023-11-06 02:52:54 +0000

    sampleで使っている関数に型ヒントを追加する

--- ss7py/Ss7Py.py
+++ ss7py/Ss7Py.py
@@ -1,14 +1,19 @@
 from enum import Enum, Flag, auto
 from Ssp import Ss7Python as Ss7
 import CommonException
+import OpenException
 
 
 class Version(Enum):
+    """処理中に使うSS7のバージョン"""
+
     LATEST = None
     V1_1_1_19 = "1.1.1.19"
 
 
 class ClearLog(Enum):
+    """処理開始時にログファイルをリセットするか追記するか"""
+
     CLEAR = 1
     NOT_CLEAR = 2
 
@@ -19,6 +24,8 @@ class PrintMember(Enum):
 
 
 class Results(Enum):
+    """解析結果のスロット番号"""
+
     RESULT_1 = "結果1"
     RESULT_2 = "結果2"
     RESULT_3 = "結果3"
@@ -27,6 +34,8 @@ class Results(Enum):
 
 
 class Save(Enum):
+    """物件データの変更を保存するか破棄するか"""
+
     SAVE = 1
     WITHOUT_SAVE = 2
 
@@ -63,6 +72,8 @@ class ConvertModel(Enum):
 
 
 class BackupData(Enum):
+    """物件データにバックアップデータが存在した場合の動作"""
+
     SAVEAS = 1
     DELETE = 2
     INTERRUPT = 3
@@ -82,21 +93,21 @@ class LinkLimitStrengthModel(Enum):
 
 
 class Ss7Data:
-    def __init__(self, data):
+    def __init__(self, data) -> None:
         self.data = data
 
     def GetInputData(self):
         pass
 
-    def GetResultData(self, param1):
-        data = self.data.GetResultData(param1)
+    def GetResultData(self, result_number: Results) -> Ss7Result:
+        data = self.data.GetResultData(result_number)
         return Ss7Result(data)
 
     def Save(self):
         pass
 
-    def Close(self, param1):
-        pass
+    def Close(self, save: Save) -> None:
+        self.data.Close(save)
 
     def Restore(self, param1):
         pass
@@ -104,8 +115,8 @@ class Ss7Data:
     def DeleteResult(self, param1):
         pass
 
-    def Calculate(self, param1, param2):
-        pass
+    def Calculate(self, result_number: Results, calculation_item: str) -> None:
+        self.data.Calculate(result_number, calculation_item)
 
     def IsCalcEnd(self, param1, param2):
         pass
@@ -120,11 +131,16 @@ class Ss7Input:
 
 
 class Ss7Result:
+    def __init__(self, result) -> None:
+        self.data = result
+
     def CreateDocument(self, param1, param2, param3):
         pass
 
-    def ExportInputCsv(self, param1, param2, param3):
-        pass
+    def ExportInputCsv(
+        self, csv_path: str, overwrite: Overwrite, symbol_duplicate: SymbolDuplicate
+    ) -> None:
+        self.data.ExportInputCsv(csv_path, overwrite, symbol_duplicate)
 
     def ExportResultCsv(self, param1, param2, param3, param4, param5):
         pass
@@ -143,7 +159,9 @@ def Init() -> None:
 
 class Ss7Py:
     @staticmethod
-    def Start(version: Version = Version.LATEST, clear_log: ClearLog = ClearLog.CLEAR):
+    def Start(
+        version: Version = Version.LATEST, clear_log: ClearLog = ClearLog.CLEAR
+    ) -> None:
         Ss7.Start(version.value, clear_log.value)
         err = Ss7.GetLastError()
         no: int = err.GetErrorNo()
@@ -156,7 +174,7 @@ class Ss7Py:
             raise CommonException.LicenseExpiredError(err)
 
     @staticmethod
-    def End(save: Save = Save.WITHOUT_SAVE):
+    def End(save: Save = Save.WITHOUT_SAVE) -> None:
         Ss7.End(save.value)
 
     @staticmethod
@@ -173,9 +191,44 @@ class Ss7Py:
         pass
 
     @staticmethod
-    def Open(param1, param2, param3) -> Ss7Data:
-        result = Ss7.Open(param1, param2, param3)
-        return Ss7Data(result)
+    def Open(path: str, overwrite: Overwrite, backupdata: BackupData) -> Ss7Data:
+        result = Ss7.Open(path, overwrite, backupdata)
+
+        if result == None:
+            err = Ss7.GetLastError()
+            no: int = err.GetErrorNo()
+
+            if no == 101:
+                raise CommonException.LicenseMissingError(err)
+            elif no == 102:
+                raise CommonException.AlreadyRunningError()
+            elif no == 107:
+                raise CommonException.LicenseExpiredError(err)
+            elif no == 1:
+                raise OpenException.FileNotFoundError(err)
+            elif no == 2:
+                raise OpenException.FileAlreadyOpenedError(err)
+            elif no == 3:
+                raise OpenException.BackupDataExistsError(err)
+            elif no == 4:
+                raise OpenException.FileVersionLowerError(err)
+            elif no == 5:
+                raise OpenException.FileVersionUpperError(err)
+            elif no == 6:
+                raise OpenException.StructureTypeRestrictedError(err)
+            elif no == 7:
+                raise OpenException.FileWriteAccessError(err)
+            elif no == 8:
+                raise OpenException.FileCannotOpenError(err)
+            elif no == 12:
+                raise OpenException.WoodStructureLicenseMissingError(err)
+            elif no == 13:
+                raise OpenException.IsolationStructureLicenseMissingError(err)
+            elif no == 15:
+                raise OpenException.PremiumLicenseMissingError(err)
+
+        else:
+            return Ss7Data(result)
 
     @staticmethod
     def GetLastError():
